7e7f755467f3b6d01845a38e6c35b31d9983a28e
Miloslav Thon <miloslav.thon@gmail.com>
2012-11-22 23:02:05 +0100

    Doplněny metody pro často používané validace.

--- RezervacniSystem.Infrastructure/Validate.cs
+++ RezervacniSystem.Infrastructure/Validate.cs
@@ -8,17 +8,54 @@ namespace RezervacniSystem.Infrastructure
 {
 	public static class Validate
 	{
-		public static void NotNull(Object obj, String message)
+		public static void NotNull(Object value, String message)
 		{
-			if (obj == null)
+			if (value == null)
 			{
-				throw new ArgumentNullException(message);
+				throw new ArgumentException(message);
+			}
+		}
+
+		public static void IsNull(Object value, String message)
+		{
+			if (value != null)
+			{
+				throw new ArgumentException(message);
 			}
 		}
 
 		public static void NotNullOrEmpty(String value, String message)
 		{
 			if (String.IsNullOrEmpty(value))
+			{
+				throw new ArgumentException(message);
+			}
+		}
+
+		public static void IsTrue(bool predicate, String message)
+		{
+			if (!predicate)
+			{
+				throw new ArgumentException(message);
+			}
+		}
+
+		public static void IsNotTrue(bool predicate, String message)
+		{
+			IsTrue(!predicate, message);
+		}
+
+		public static void AreEqual(Object a, Object b, String message)
+		{
+			if (!Object.Equals(a, b))
+			{
+				throw new ArgumentException(message);
+			}
+		}
+
+		public static void AreNotEqual(Object a, Object b, String message)
+		{
+			if (Object.Equals(a, b))
 			{
 				throw new ArgumentException(message);
 			}
