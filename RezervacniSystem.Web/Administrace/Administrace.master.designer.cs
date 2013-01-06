75d8ebb7dbdaa831d3508d05c442dca3e92977ef
Miloslav Thon <miloslav.thon@gmail.com>
2013-01-06 21:21:28 +0100

    Skryta nevyužitá navigace v administrátorské části aplikace.

--- RezervacniSystem.Web/Administrace/Administrace.master.designer.cs
+++ RezervacniSystem.Web/Administrace/Administrace.master.designer.cs
@@ -13,24 +13,6 @@ namespace RezervacniSystem.Web.Administrace {
     public partial class Administrace {
         
         /// <summary>
-        /// A1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor A1;
-        
-        /// <summary>
-        /// A2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor A2;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
